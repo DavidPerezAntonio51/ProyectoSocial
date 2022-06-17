469d70df283748b118b67c54b1641b3d1bf90809
JMTost <max.55@live.com.mx>
2022-06-17 15:38:51 -0500

    modificación de los componentes de conicionales, creación de las soluciones

--- aprendamos-java/src/Paginas/Temas/Condicionales/CondicionalIf.js
+++ aprendamos-java/src/Paginas/Temas/Condicionales/CondicionalIf.js
@@ -5,6 +5,7 @@ import CondicionalesInicial from "./CondicionalesInicial";
 import SiguienteTema from "../../../Componentes/ComponentesDeLosTemas/BotonSiguienteTema";
 import VolverAlMenu from "../../../Componentes/ComponentesDeLosTemas/BotonVolver";
 import UserContext from "../../../Contextos/UserContext";
+import MostrarRespuesta from "../../../Componentes/ComponentesDeLosTemas/MostrarResppuesta";
 import { useContext } from "react";
 
 function createData(item, repre, sintaxis){
@@ -71,9 +72,9 @@ function CondicionalIf(){
                                     <br/>
                                     {tab}{"public static void main(String[] args){"}
                                     <br />
-                                    {tab}{'int numero = 2;'}<span className="comentario">{'//Inicializamos el valor de numero con 2'}</span>
+                                    {tab}{'int numero = 2;'}&nbsp;&nbsp;<span className="comentario">{'//Inicializamos el valor de numero con 2'}</span>
                                     <br />
-                                    {tab}{tab}{"if(numero > 1){"}<span className="comentario">{"//preguntamos ¿2 (numero) es mayot a 1?"}</span>
+                                    {tab}{tab}{"if(numero > 1){"}&nbsp;&nbsp;<span className="comentario">{"//preguntamos ¿2 (numero) es mayor a 1?"}</span>
                                     <br />
                                     {tab}{tab}{tab}{`//CIERTO`}
                                     <br />
@@ -141,8 +142,114 @@ function CondicionalIf(){
                         <Typography variant="body1" component="p">
                             <ol>
                                 <li>Escribe un programa que le pregunte al usuario su edad, a partir de esta imprimir en pantalla si es mayor a 18 y menor a 50 años.</li>
+                                {tab}<MostrarRespuesta>
+                                                <Typography variant="h6" sx={{paddingX:10}}>
+                                                    <Box sx={{maxWidth:"calc(90vw - 32px)", overflowX:"auto", overflowY:"unset", whiteSpace:"nowrap", backgroundColor:"#111111", color:"#e1e7f2", paddingY:3, paddingTop:3, paddingX:3, fontSize:12}}>
+                                                        {"import java.util.Scanner;"}
+                                                        <br />
+                                                        {"public class Main{"}
+                                                        <br/>
+                                                        {tab}{"public static void main(String[] args){"}
+                                                        <br />
+                                                        {tab}{'int numero;'}&nbsp;&nbsp;<span className="comentario">{'//creamos la variable que utilizaremos para almacenar el dato'}</span>
+                                                        <br />
+                                                        {tab}{'Scanner entrada = new Scanner(System.in);'}&nbsp;&nbsp;<span className="comentario">{'//creamos el objeto para obtener la entrada del usuario'}</span>
+                                                        <br />
+                                                        {tab}{'System.out.println("Ingrese su edad: ");'}&nbsp;&nbsp;<span className="comentario">{'//Imprimimos la indicación que el usuario debe hacer'}</span>
+                                                        <br />
+                                                        {tab}{'edadUsuario = entrada.nextInt();'}&nbsp;&nbsp;<span className="comentario">{'//Almacenamos la entrada del usuario'}</span>
+                                                        <br />
+                                                        {tab}{tab}{"if(edadUusario > 18 && edadUsaurio < 50){"}&nbsp;&nbsp;<span className="comentario">{"//preguntamos ¿la entrada esta en este rango?"}</span>
+                                                        <br />
+                                                        {tab}{tab}{tab}{`//CIERTO`}
+                                                        <br />
+                                                        {tab}{tab}{tab}{`//imprimos este mensaje`}
+                                                        <br />
+                                                        {tab}{tab}{tab}{`System.out.println("Tu edad es aceptada en los criterios seleccionados");`}
+                                                        <br />
+                                                        {tab}{tab}{`}`}
+                                                        <br />
+                                                        {tab}{`}`}
+                                                        <br />
+                                                        {`}`}
+                                                    </Box>
+                                                </Typography>
+                                        </MostrarRespuesta>
+                                    
                                 <li>Escribe un programa que pida al usuario 2 números y que imprima en pantalla la diferencia de estos si es menor a cero.</li>
+                               {tab}<MostrarRespuesta>
+                                                <Typography variant="h6" sx={{paddingX:10}}>
+                                                    <Box sx={{maxWidth:"calc(90vw - 32px)", overflowX:"auto", overflowY:"unset", whiteSpace:"nowrap", backgroundColor:"#111111", color:"#e1e7f2", paddingY:3, paddingTop:3, paddingX:3, fontSize:12}}>
+                                                        {"import java.util.Scanner;"}
+                                                        <br />
+                                                        {"public class Main{"}
+                                                        <br/>
+                                                        {tab}{"public static void main(String[] args){"}
+                                                        <br />
+                                                        {tab}{'int a, b, resta;'}&nbsp;&nbsp;<span className="comentario">{'//creamos las variables que utilizaremos para el ejemplo'}</span>
+                                                        <br />
+                                                        {tab}{'Scanner entrada = new Scanner(System.in);'}&nbsp;&nbsp;<span className="comentario">{'//creamos el objeto para obtener la entrada del usuario'}</span>
+                                                        <br />
+                                                        {tab}{'System.out.println("Ingrese el primer numero: ");'}&nbsp;&nbsp;<span className="comentario">{'//Imprimimos la indicación que el usuario debe hacer'}</span>
+                                                        <br />
+                                                        {tab}{'a = entrada.nextInt();'}&nbsp;&nbsp;<span className="comentario">{'//Almacenamos la entrada del usuario'}</span>
+                                                        <br />
+                                                        {tab}{'System.out.println("Ingrese el segundo numero: ");'}&nbsp;&nbsp;<span className="comentario">{'//Imprimimos la indicación que el usuario debe hacer'}</span>
+                                                        <br />
+                                                        {tab}{'b = entrada.nextInt();'}&nbsp;&nbsp;<span className="comentario">{'//Almacenamos la entrada del usuario'}</span>
+                                                        <br />
+                                                        {tab}{'resta = a - b;'}&nbsp;&nbsp;<span className="comentario">{'//Realizamos la operación requerida'}</span>
+                                                        <br />
+                                                        {tab}{tab}{"if(resta > 0){"}&nbsp;&nbsp;<span className="comentario">{"//preguntamos ¿la resta es positiva?"}</span>
+                                                        <br />
+                                                        {tab}{tab}{tab}{`//CIERTO`}
+                                                        <br />
+                                                        {tab}{tab}{tab}{`//imprimos este mensaje`}
+                                                        <br />
+                                                        {tab}{tab}{tab}{`System.out.println("La diferencia de tus numeros es positiva: "+resta);`}
+                                                        <br />
+                                                        {tab}{tab}{`}`}
+                                                        <br />
+                                                        {tab}{`}`}
+                                                        <br />
+                                                        {`}`}
+                                                    </Box>
+                                                </Typography>
+                                        </MostrarRespuesta>
                                 <li>Escribe un programa que pida al usuario un número, debe imprimir en pantalla si el numero es par, mayor a 2 y menor a 100</li>
+                                {tab}<MostrarRespuesta>
+                                                <Typography variant="h6" sx={{paddingX:10}}>
+                                                    <Box sx={{maxWidth:"calc(90vw - 32px)", overflowX:"auto", overflowY:"unset", whiteSpace:"nowrap", backgroundColor:"#111111", color:"#e1e7f2", paddingY:3, paddingTop:3, paddingX:3, fontSize:12}}>
+                                                        {"import java.util.Scanner;"}
+                                                        <br />
+                                                        {"public class Main{"}
+                                                        <br/>
+                                                        {tab}{"public static void main(String[] args){"}
+                                                        <br />
+                                                        {tab}{'int numero;'}&nbsp;&nbsp;<span className="comentario">{'//creamos la variable para obtener el numero'}</span>
+                                                        <br />
+                                                        {tab}{'Scanner entrada = new Scanner(System.in);'}&nbsp;&nbsp;<span className="comentario">{'//creamos el objeto para obtener la entrada del usuario'}</span>
+                                                        <br />
+                                                        {tab}{'System.out.println("Ingrese el numero: ");'}&nbsp;&nbsp;<span className="comentario">{'//Imprimimos la indicación que el usuario debe hacer'}</span>
+                                                        <br />
+                                                        {tab}{'numero = entrada.nextInt();'}&nbsp;&nbsp;<span className="comentario">{'//Almacenamos la entrada del usuario'}</span>
+                                                        <br />
+                                                        {tab}{tab}{"if( (numero % 2 == 0) && (numero > 2 && numero < 100) ){"}&nbsp;&nbsp;<span className="comentario">{"//preguntamos ¿si el numero es par y si corresponde al rango?"}</span>
+                                                        <br />
+                                                        {tab}{tab}{tab}{`//CIERTO`}
+                                                        <br />
+                                                        {tab}{tab}{tab}{`//imprimos este mensaje`}
+                                                        <br />
+                                                        {tab}{tab}{tab}{`System.out.println("El numero es par y esta entre 2 y 100, tu entrada: "+numero);`}
+                                                        <br />
+                                                        {tab}{tab}{`}`}
+                                                        <br />
+                                                        {tab}{`}`}
+                                                        <br />
+                                                        {`}`}
+                                                    </Box>
+                                                </Typography>
+                                        </MostrarRespuesta>
                             </ol>
                         </Typography>
                         <Grid container justifyContent="space-between" alignItems="center" sx={{paddingTop:3}}>
